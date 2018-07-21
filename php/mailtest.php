c836fdc88cf0d8d71eba1dfa1f6ca9a89f2340a7
Baokhoa Vu <tealkrysta92@gmail.com>
2018-07-21 12:34:38 -0700

    test php mail

--- php/mailtest.php
+++ php/mailtest.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL);
+
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
@@ -13,12 +15,6 @@ require '../src/Exception.php'
 require '../src/PHPMailer.php';
 require '../src/SMTP.php';
 require '../src/POP3.php';
-
-if($mail->send()){
-    echo 'Message has been sent';
-} else {
-    error_reporting(E_ALL); ini_set('display_errors', 1);
-}
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 
