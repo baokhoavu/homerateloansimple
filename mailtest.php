a5749cd9e4c42124ef5662364a5fa7a96194b2f3
Baokhoa Vu <tealkrysta92@gmail.com>
2018-07-21 13:45:33 -0700

    test php

--- mailtest.php
+++ mailtest.php
@@ -12,7 +12,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 // use PHPMailer\PHPMailer\POP3;
 
 // require '/vendor/autoload.php';
-require 'src/Exception.php';
+// require 'src/Exception.php';
 require 'src/PHPMailer.php';
 require 'src/SMTP.php';
 require 'src/POP3.php';
