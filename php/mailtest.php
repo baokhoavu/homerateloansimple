bab00fc37e88ce8bcc2ea0ff948fea26631f510f
baokhoavu <tealkrysta92@gmail.com>
2018-08-16 14:39:15 -0700

    Update mailtest.php

--- php/mailtest.php
+++ php/mailtest.php
@@ -25,8 +25,8 @@ error_reporting(E_ALL);
 // $mail->isSMTP();                                      // Set mailer to use SMTP
 // $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 // $mail->SMTPAuth = true;                               // Enable SMTP authentication
-// $mail->Username = 'testbaokhoa@gmail.com';                 // SMTP username
-// $mail->Password = 'test123test123';                           // SMTP password
+// $mail->Username = '';                 // SMTP username
+// $mail->Password = '';                           // SMTP password
 // $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 // $mail->Port = 587;                                    // TCP port to connect to
 
